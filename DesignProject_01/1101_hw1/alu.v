de4a40bd3e5fd53faa6b9b99834b9bd064a96af3
Marshars <rosadim@mcmaster.ca>
2026-01-14 18:05:41 -0500

    first commit

---
+++ DesignProject_01/1101_hw1/alu.v
@@ -0,0 +1,65 @@
+module alu (
+    input               i_clk,
+    input               i_rst_n,
+    input               i_valid,
+    input signed [11:0] i_data_a,
+    input signed [11:0] i_data_b,
+    input        [2:0]  i_inst,
+    output              o_valid,
+    output       [11:0] o_data,
+    output              o_overflow
+);
+    
+// ---------------------------------------------------------------------------
+// Wires and Registers
+// ---------------------------------------------------------------------------
+reg  [11:0] o_data_w, o_data_r;
+reg         o_valid_w, o_valid_r;
+reg         o_overflow_w, o_overflow_r;
+// ---- Add your own wires and registers here if needed ---- //
+
+
+
+
+// ---------------------------------------------------------------------------
+// Continuous Assignment
+// ---------------------------------------------------------------------------
+assign o_valid = o_valid_r;
+assign o_data = o_data_r;
+assign o_overflow = o_overflow_r;
+// ---- Add your own wire data assignments here if needed ---- //
+
+
+
+
+// ---------------------------------------------------------------------------
+// Combinational Blocks
+// ---------------------------------------------------------------------------
+// ---- Write your conbinational block design here ---- //
+always@(*) begin
+    o_data_w = ;
+    o_overflow_w = ;
+    o_valid_w = ;
+end
+
+
+
+
+// ---------------------------------------------------------------------------
+// Sequential Block
+// ---------------------------------------------------------------------------
+// ---- Write your sequential block design here ---- //
+always@(posedge i_clk or negedge i_rst_n) begin
+    if(!i_rst_n) begin
+        o_data_r <= 0;
+        o_overflow_r <= 0;
+        o_valid_r <= 0;
+    end else begin
+        o_data_r <= o_data_w;
+        o_overflow_r <= o_overflow_w;
+        o_valid_r <= o_valid_w;
+    end
+end
+
+
+endmodule
