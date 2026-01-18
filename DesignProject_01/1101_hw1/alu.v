f2afa82ec8e300bf2969772648fea1ce9c79e144
Sharda Kotru <sharda.kotru@gmail.com>
2026-01-18 00:23:45 -0500

    absolute max and mean workinggit add alu.v!

--- DesignProject_01/1101_hw1/alu.v
+++ DesignProject_01/1101_hw1/alu.v
@@ -16,8 +16,9 @@ module alu (
 reg  signed[11:0] o_data_w, o_data_r;
 reg         o_valid_w, o_valid_r;
 reg         o_overflow_w, o_overflow_r;
-reg [11:0] i_data_a_reg, i_data_b_reg;
 // ---- Add your own wires and registers here if needed ---- //
+reg [11:0] a_abs, b_abs;
+reg [12:0] long_sum;
 
 // ---------------------------------------------------------------------------
 // Continuous Assignment
@@ -37,21 +38,43 @@ always@(*) begin
 	
 	case (i_inst)
 		3'b000: begin   //ADD
-				o_data_w = $signed(i_data_a )+ $signed(i_data_b);
-				o_overflow_w = (~o_data_w[11] & i_data_a[11] & i_data_b[11]) | (o_data_w[11] & (~i_data_a[11]) & (~i_data_b[11]));
-			end
+			o_data_w = $signed(i_data_a )+ $signed(i_data_b);
+			o_overflow_w = (~o_data_w[11] & i_data_a[11] & i_data_b[11]) | (o_data_w[11] & (~i_data_a[11]) & (~i_data_b[11]));
+		end
 
 		3'b001: begin  //SUBTRACT
-				o_data_w = $signed(i_data_a) - $signed(i_data_b);
-				o_overflow_w = ((i_data_a[11] ^ i_data_b[11]) && ((i_data_b[11] && o_data_w[11])) | (~i_data_b[11] && ~o_data_w[11]));
-			end
+			o_data_w = $signed(i_data_a) - $signed(i_data_b);
+			o_overflow_w = ((i_data_a[11] ^ i_data_b[11]) && ((i_data_b[11] && o_data_w[11])) | (~i_data_b[11] && ~o_data_w[11]));
+		end
+		
+		3'b010: begin //MULTIPLY
+		
+		end		
+		
+		3'b011: begin //MAC
+
+		end	
+		
 		3'b100: begin  //bitwise XNOR
-				o_data_w = i_data_a ~^ i_data_b;
-				o_overflow_w = 0;  //no overflow possible
+			o_data_w = i_data_a ~^ i_data_b;
+			o_overflow_w = 0;  //no overflow possible
 		end
 		3'b101: begin //ReLU (checking the sign bit)
-				o_data_w = i_data_a[11] ? 12'b0 : i_data_a[11:0];
-				o_overflow_w=0; //no overflow possible
+			o_data_w = i_data_a[11] ? 12'b0 : i_data_a[11:0];
+			o_overflow_w=0; //no overflow possible
+		end
+		
+		3'b110: begin  //MEAN
+			 long_sum = ($signed(i_data_a) + $signed(i_data_b))>>> 1;
+			 o_data_w = long_sum[11:0]; //intermediate sum used to store extra bit, then truncated.
+			 o_overflow_w = 0;
+		end
+
+		3'b111: begin //ABS MAX --> max(|a|,|b|)
+			a_abs = i_data_a[11] ? ((~i_data_a[11:0])+1) : i_data_a[11:0];
+			b_abs = i_data_b[11] ? ((~i_data_b[11:0])+1) : i_data_b[11:0];
+			o_data_w = (a_abs > b_abs) ? a_abs : b_abs;
+			o_overflow_w = 0;
 		end	
 	endcase
 end
